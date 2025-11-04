d13a4bc6ffbf156b1485466dc7fd0b3bd57ad648
akg_frejm <ak.grabowsk3@student.uw.edu.pl>
2025-11-04 11:46:05 +0100

    Made example independent from pyZULF, does the same as before just now enclosed.

--- example.py
+++ example.py
@@ -1,30 +1,66 @@
 import numpy as np
 import signal_instructions as si
-import pyZULF
 
 example_shapes = {
     "sin_strange": si.Shape( 
     lambda x, a=0.0, b=1.0: b * ( np.sin( np.pi * x ) - a * np.sin( np.pi * x * 2 ) / 2.0 ) / (1+np.abs(a)/2),
     duration = 10.0 
 ),
-    "gate_from_file": si.shape_from_file( pyZULF.path_to_pyZULF + "example/dane.dat" )
+    "gate_from_file": si.shape_from_file( "example/dane.dat" )
 }
 
-si.load_parameters( new_shapes=example_shapes )
+channels = { "x": 0, "y": 1 }
+
+calibration = { "x": 1., "y": 1. }
+
+input_variables={ "skew": 3.0, "amp": 10.0 }
+
+si.load_parameters( 
+    new_shapes = example_shapes, 
+    new_channels = channels, 
+    new_field_scaling=calibration,
+    new_variables=input_variables
+)
 
 if __name__ == "__main__":
 
-    with open( pyZULF.path_to_pyZULF + "example/instructions.txt" ) as file:
-        instr = file.read()
+    file = open( "example/instructions.txt" )
+    instr = file.read()
+    file.close()
+
+    dt = 0.01
     
-    signals, schedules = pyZULF.labview_impulse_interface( 
-        instructions_txt=instr,
-        input_variables=[ 
-            ("skew", 3.0),
-            ("amp", 10.0)
-        ],
-        device_list=[("dev",1), ("dev",2)], dt = 0.01 )
-    
-    commands = si.get_commands( instr )
+    si.load_parameters( new_variables = input_variables )
+
+    commands = si.get_commands_from_instructions( instr )
     impulses, total_duration = si.gather_impulses( commands )
+
+    signal_list = []
+    time_list = []
+
+    for ch in [0,1]:
+
+        schedule = si.get_gate_schedule( impulses, ch )
+        if len(schedule) == 0:
+            
+            signal_list.append( [0,0] )
+            time_list.append( [0,total_duration] )
+
+            continue
+
+        t_start = schedule[0]
+        duration = schedule[-1]-t_start
+
+        t = t_start + np.arange( 1, int(duration/dt) + 1 )*dt
+        V = si.calculate_final_voltages( impulses, t, ch )
+
+        V = np.insert( V, [0,0], [0.0,0.0] )
+        t = np.insert( t, [0,0], [t[0]-dt,t[0]] )
+        t[2] += 500e-12
+        
+        signal_list.append( V )
+        time_list.append( t )
+
+    signals = list( zip(time_list, signal_list) )
+    
     si.draw_instructions( impulses, signals )
